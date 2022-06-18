8f2f608f8e030481a92653488b63d075f3c54449
DESKTOP-DTMTFUL\thais <thais.soares@outlook.com>
2022-06-18 02:26:23 -0300

    add operadores

---
+++ Aula02/OperadoresNumericos.py
@@ -0,0 +1,26 @@
+#Opeções com string
+
+texto = 'um texto qualquer'
+outro_texto = '10'
+print(texto* 10) # ele repete 10 vezes
+print(texto + outro_texto) #ele junta os txt, faz uma concatenação (lembrando que não se concatena, string com inteiro)
+# no caso de txt com txt e divisão também não são aceitáveis
+
+contador = 0
+contador = contador + 1
+print(contador)
+contador = contador + 1 # acumular
+print(contador)
+
+#com outra notação
+contador += 1
+print(contador)
+
+contador *= 2 # com multiplicação
+print(contador)
+
+contador /= 2 # vcom divisão, sendo que o tipo modifica para un tipo flutuante -> float
+print(contador)
+
+
+
